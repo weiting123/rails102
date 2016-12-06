63c33dd9645cd42ed2a9af83daa029c00db5db3f
weiting <wt572051341@163.com>
2016-12-06 18:14:29 +0800

    浏览

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,11 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group=Group.find(params[:id])
+  end
+
+  # 新增
   def create
     @group=Group.new(group_params)
     @group.save
